a51dd6140920d8180c10c1dbca21a8d03c9f7bde
PhilSoGooood <peelhw@gmail.com>
2022-10-18 19:26:10 +0900

    :tada: 프로젝트 초기 설정

---
+++ noriter/src/main/java/com/codewarts/noriter/NoriterApplication.java
@@ -0,0 +1,13 @@
+package com.codewarts.noriter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoriterApplication.class, args);
+	}
+
+}
---
+++ noriter/src/test/java/com/codewarts/noriter/NoriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewarts.noriter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
