1c03f258e4d768b0aa037921087fe00b1c9b3da4
PhilSoGooood <peelhw@gmail.com>
2022-11-16 18:18:43 +0900

    :sparkles: 질문 상세 조회 기능 구현
    - 추후 로그인 여부에 따라 내가 wish를 추가한 질문인지 여부를 보여줄 예정

---
+++ noriter/src/main/java/com/codewarts/noriter/article/domain/dto/question/QuestionDetailResponse.java
@@ -0,0 +1,46 @@
+package com.codewarts.noriter.article.domain.dto.question;
+
+import com.codewarts.noriter.article.domain.Hashtag;
+import com.codewarts.noriter.article.domain.Question;
+import com.codewarts.noriter.comment.domain.dto.CommentResponse;
+import com.codewarts.noriter.common.domain.dto.WriterInfoResponse;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.Getter;
+
+@Getter
+public class QuestionDetailResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final WriterInfoResponse writer;
+    private final List<String> hashtag;
+    private final LocalDateTime writtenTime;
+    private final LocalDateTime editedTime;
+    private final int wishCount;
+    private final List<CommentResponse> comment;
+    private final boolean completed;
+
+    public QuestionDetailResponse(Question question) {
+        this.id = question.getId();
+        this.title = question.getTitle();
+        this.content = question.getContent();
+        this.writer = WriterInfoResponse.from(question.getWriter());
+        this.hashtag = question.getHashtags().stream()
+            .map(Hashtag::getContent)
+            .collect(Collectors.toList());
+        this.writtenTime = question.getWrittenTime();
+        this.editedTime = question.getEditedTime();
+        this.wishCount = question.getWishList().size();
+        this.comment = question.getComments().stream()
+            .map(CommentResponse::new)
+            .collect(Collectors.toList());
+        this.completed = question.isCompleted();
+    }
+
+    public static QuestionDetailResponse from(Question question) {
+        return new QuestionDetailResponse(question);
+    }
+}
--- noriter/src/main/java/com/codewarts/noriter/article/service/QuestionService.java
+++ noriter/src/main/java/com/codewarts/noriter/article/service/QuestionService.java
@@ -1,9 +1,11 @@
 package com.codewarts.noriter.article.service;
 
 import com.codewarts.noriter.article.domain.Question;
+import com.codewarts.noriter.article.domain.dto.question.QuestionDetailResponse;
 import com.codewarts.noriter.article.domain.dto.question.QuestionPostRequest;
 import com.codewarts.noriter.article.domain.dto.question.QuestionResponse;
 import com.codewarts.noriter.article.exception.member.NoSuchMemberException;
+import com.codewarts.noriter.article.exception.question.NoSuchQuestionException;
 import com.codewarts.noriter.article.repository.ArticleRepository;
 import com.codewarts.noriter.common.domain.Member;
 import com.codewarts.noriter.common.repository.MemberRepository;
@@ -21,7 +23,7 @@ public class QuestionService {
     private final ArticleRepository questionRepository;
     private final MemberRepository memberRepository;
 
-    // 질문 등록하기
+    // 질문 등록 기능
     @Transactional
     public Long add(QuestionPostRequest request, Long memberId) {
         Member writer = memberRepository.findById(memberId).orElseThrow(NoSuchMemberException::new);
@@ -31,11 +33,18 @@ public class QuestionService {
         return savedQuestion.getId();
     }
 
-    // 질문 조회하기
-    public List<QuestionResponse> findQuestionList(Boolean status) {
+    // 질문 조회 기능
+    public List<QuestionResponse> findList(Boolean status) {
         if (status == null) {
             return  questionRepository.findAllQuestion().stream().map(QuestionResponse::new).collect(Collectors.toList());
         }
         return questionRepository.findQuestionByCompleted(status).stream().map(QuestionResponse::new).collect(Collectors.toList());
+    }
+
+    // 질문 상세 조회 기능
+    public QuestionDetailResponse findDetail(Long id) {
+        Question question = questionRepository.findQuestionById(id)
+            .orElseThrow(NoSuchQuestionException::new);
+        return QuestionDetailResponse.from(question);
     }
 }
