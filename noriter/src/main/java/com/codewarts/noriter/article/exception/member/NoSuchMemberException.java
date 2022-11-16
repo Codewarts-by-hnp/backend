5a00808bf9206723fd07008204346f7866b66775
PhilSoGooood <peelhw@gmail.com>
2022-11-16 18:16:44 +0900

    :safety_vest: 회원과 질문 글이 존재하지 않는 경우 예외 추가

--- noriter/src/main/java/com/codewarts/noriter/article/exception/member/NoSuchMemberException.java
+++ noriter/src/main/java/com/codewarts/noriter/article/exception/member/NoSuchMemberException.java
@@ -2,4 +2,7 @@ package com.codewarts.noriter.article.exception.member;
 
 public class NoSuchMemberException extends RuntimeException {
 
+    public NoSuchMemberException() {
+        super("존재하지 않는 회원입니다.");
+    }
 }
