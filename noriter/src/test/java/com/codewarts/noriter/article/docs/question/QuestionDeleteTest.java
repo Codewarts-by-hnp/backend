13e66ee44a5650453e874c5da61bb4bcdd618fe8
PhilSoGooood <peelhw@gmail.com>
2022-12-24 22:37:17 +0900

    :white_check_mark: 질문 게시판 삭제 기능 실패 케이스 테스트
    - AccessToken이 비어있거나 변조된 경우
    - 해당 memberId가 존재하지 않는 경우
    - Path Variable이 비어있는 경우
    - Article Id가 존재 및 유효하지 않는 경우
    - 글쓴이가 아닌 다른 사람이 삭제를 요청한 경우

---
+++ noriter/src/test/java/com/codewarts/noriter/article/docs/question/QuestionDeleteTest.java
@@ -0,0 +1,225 @@
+package com.codewarts.noriter.article.docs.question;
+
+import static com.codewarts.noriter.exception.type.ArticleExceptionType.ARTICLE_NOT_MATCHED_WRITER;
+import static com.codewarts.noriter.exception.type.AuthExceptionType.EMPTY_ACCESS_TOKEN;
+import static com.codewarts.noriter.exception.type.AuthExceptionType.TAMPERED_ACCESS_TOKEN;
+import static com.codewarts.noriter.exception.type.MemberExceptionType.MEMBER_NOT_FOUND;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONNECTION;
+import static org.springframework.http.HttpHeaders.CONTENT_LENGTH;
+import static org.springframework.http.HttpHeaders.DATE;
+import static org.springframework.http.HttpHeaders.HOST;
+import static org.springframework.http.HttpHeaders.TRANSFER_ENCODING;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.removeHeaders;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
+
+import com.codewarts.noriter.auth.jwt.JwtProvider;
+import com.codewarts.noriter.exception.type.ArticleExceptionType;
+import com.codewarts.noriter.exception.type.CommonExceptionType;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.jdbc.Sql;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@ExtendWith({RestDocumentationExtension.class})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql("classpath:/data.sql")
+class QuestionDeleteTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    protected JwtProvider jwtProvider;
+
+    protected RequestSpecification documentationSpec;
+
+    @BeforeEach
+    void setup(RestDocumentationContextProvider restDocumentation) {
+        RestAssured.port = port;
+        documentationSpec = new RequestSpecBuilder()
+            .addFilter(
+                documentationConfiguration(restDocumentation)
+                    .operationPreprocessors()
+                    .withRequestDefaults(
+                        prettyPrint(),
+                        removeHeaders(HOST, CONTENT_LENGTH))
+                    .withResponseDefaults(
+                        prettyPrint(),
+                        removeHeaders(CONTENT_LENGTH, CONNECTION, DATE, TRANSFER_ENCODING,
+                            "Keep-Alive",
+                            HttpHeaders.VARY))
+            )
+            .build();
+    }
+
+    @Test
+    void 글을_삭제한다() {
+        String accessToken = jwtProvider.issueAccessToken(1L);
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("id", 6)
+
+            .when()
+            .delete("/community/question/{id}")
+
+            .then()
+            .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void Access_Token이_비어있는_경우_예외_발생() {
+        String accessToken = " ";
+
+        Map<String, Object> requestBody = Map.of("title", "질문있어요",
+            "content", "스프링 어려워요", "hashtags",
+            List.of("질문게시판", "고수가 되고싶어요", "코린이"));
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("id", 1)
+            .body(requestBody)
+
+            .when()
+            .delete("/community/question/{id}")
+
+            .then()
+            .statusCode(EMPTY_ACCESS_TOKEN.getStatus().value())
+            .body("errorCode", equalTo(EMPTY_ACCESS_TOKEN.getErrorCode()))
+            .body("message", equalTo(EMPTY_ACCESS_TOKEN.getErrorMessage()));
+    }
+
+    @Test
+    void Access_Token이_변조된_경우_예외_발생() {
+        String accessToken = jwtProvider.issueAccessToken(1L) + "123";
+
+        Map<String, Object> requestBody = Map.of("title", "질문있어요",
+            "content", "스프링 어려워요", "hashtags",
+            List.of("질문게시판", "고수가 되고싶어요", "코린이"));
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("id", 1)
+            .body(requestBody)
+
+            .when()
+            .delete("/community/question/{id}")
+
+            .then()
+            .statusCode(TAMPERED_ACCESS_TOKEN.getStatus().value())
+            .body("errorCode", equalTo(TAMPERED_ACCESS_TOKEN.getErrorCode()))
+            .body("message", equalTo(TAMPERED_ACCESS_TOKEN.getErrorMessage()));
+    }
+
+    @Test
+    void 존재하지_않는_회원인_경우_예외가_발생한다() {
+        String accessToken = jwtProvider.issueAccessToken(99999L);
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("id", 1)
+
+            .when()
+            .delete("/community/question/{id}")
+
+            .then()
+            .statusCode(MEMBER_NOT_FOUND.getStatus().value())
+            .body("errorCode", equalTo(MEMBER_NOT_FOUND.getErrorCode()))
+            .body("message", equalTo(MEMBER_NOT_FOUND.getErrorMessage()));
+    }
+
+    @Test
+    void Path_Variable이_없는_경우_예외를_발생시킨다() {
+        String accessToken = jwtProvider.issueAccessToken(1L);
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("id", "  ")
+
+            .when()
+            .delete("/community/question/{id}")
+
+            .then()
+            .statusCode(CommonExceptionType.INVALID_REQUEST.getStatus().value())
+            .body("errorCode", equalTo(CommonExceptionType.INVALID_REQUEST.getErrorCode()))
+            .body("message", equalTo("questionRemove.id: ID가 비어있습니다."));
+    }
+
+    @Test
+    void id가_유효하지_않는_경우_예외를_발생시킨다() {
+        String accessToken = jwtProvider.issueAccessToken(1L);
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("id", -1)
+
+            .when()
+            .delete("/community/question/{id}")
+
+            .then()
+            .statusCode(CommonExceptionType.INVALID_REQUEST.getStatus().value())
+            .body("errorCode", equalTo(CommonExceptionType.INVALID_REQUEST.getErrorCode()))
+            .body("message", equalTo("questionRemove.id: 게시글 ID는 양수이어야 합니다."));
+    }
+
+    @Test
+    void id가_존재하지_않는_경우_예외를_발생시킨다() {
+        String accessToken = jwtProvider.issueAccessToken(1L);
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("id", 999999)
+
+            .when()
+            .delete("/community/question/{id}")
+
+            .then()
+            .statusCode(ArticleExceptionType.ARTICLE_NOT_FOUND.getStatus().value())
+            .body("errorCode", equalTo(ArticleExceptionType.ARTICLE_NOT_FOUND.getErrorCode()))
+            .body("message", equalTo(ArticleExceptionType.ARTICLE_NOT_FOUND.getErrorMessage()));
+    }
+
+    @Test
+    void 작성자가_일치하지_않는_경우_예외가_발생한다() {
+        String accessToken = jwtProvider.issueAccessToken(2L);
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("id", 6)
+
+            .when()
+            .delete("/community/question/{id}")
+
+            .then()
+            .statusCode(ARTICLE_NOT_MATCHED_WRITER.getStatus().value())
+            .body("errorCode", equalTo(ARTICLE_NOT_MATCHED_WRITER.getErrorCode()));
+    }
+}
