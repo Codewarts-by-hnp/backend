abdcdef7717f6b63975a38cb4f033b77beaeb634
PhilSoGooood <peelhw@gmail.com>
2023-02-22 13:50:53 +0900

    :white_check_mark: 댓글 등록 기능 테스트
    - 엑세스 토큰 없이 요청이 온 경우
    - 엑세스 토큰이 변조된 경우
    - token의 id에 해당하는 회원이 없는 경우
    - 요청시 필수 필드값이 비어있을 경우
    - article id가 유효하지 않은 경우(null, 음수인 경우)
    - article id가 존재하지 않는 경우

--- noriter/src/test/java/com/codewarts/noriter/article/docs/comment/CommentCreateTest.java
+++ noriter/src/test/java/com/codewarts/noriter/article/docs/comment/CommentCreateTest.java
@@ -1,6 +1,12 @@
 package com.codewarts.noriter.article.docs.comment;
 
+import static com.codewarts.noriter.exception.type.ArticleExceptionType.ARTICLE_NOT_FOUND;
+import static com.codewarts.noriter.exception.type.AuthExceptionType.EMPTY_ACCESS_TOKEN;
+import static com.codewarts.noriter.exception.type.AuthExceptionType.TAMPERED_ACCESS_TOKEN;
+import static com.codewarts.noriter.exception.type.CommonExceptionType.INVALID_REQUEST;
+import static com.codewarts.noriter.exception.type.MemberExceptionType.MEMBER_NOT_FOUND;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpHeaders.CONNECTION;
 import static org.springframework.http.HttpHeaders.CONTENT_LENGTH;
@@ -76,14 +82,186 @@ class CommentCreateTest {
         given(documentationSpec)
             .contentType(APPLICATION_JSON_VALUE)
             .header(AUTHORIZATION, accessToken)
+            .pathParam("articleId", 1)
             .body(requestBody)
 
             .when()
-            .post("/1/comment")
+            .post("/{articleId}/comment")
 
             .then()
             .statusCode(HttpStatus.OK.value());
     }
 
+    @Test
+    void Access_Token이_비어있는_경우_예외_발생() {
+        String accessToken = " ";
 
+        Map<String, Object> requestBody = Map.of("content", "댓글이에요",
+            "secret", "true");
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("articleId", 1)
+            .body(requestBody)
+
+            .when()
+            .post("/{articleId}/comment")
+
+            .then()
+            .statusCode(EMPTY_ACCESS_TOKEN.getStatus().value())
+            .body("errorCode", equalTo(EMPTY_ACCESS_TOKEN.getErrorCode()))
+            .body("message", equalTo(EMPTY_ACCESS_TOKEN.getErrorMessage()));
+    }
+
+    @Test
+    void Access_Token이_변조된_경우_예외_발생() {
+        String accessToken = jwtProvider.issueAccessToken(1L) + "123";
+
+        Map<String, Object> requestBody = Map.of("content", "댓글이에요",
+            "secret", "true");
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("articleId", 1)
+            .body(requestBody)
+
+            .when()
+            .post("/{articleId}/comment")
+
+            .then()
+            .statusCode(TAMPERED_ACCESS_TOKEN.getStatus().value())
+            .body("errorCode", equalTo(TAMPERED_ACCESS_TOKEN.getErrorCode()))
+            .body("message", equalTo(TAMPERED_ACCESS_TOKEN.getErrorMessage()));
+    }
+
+    @Test
+    void 존재하지_않는_회원인_경우_예외가_발생한다() {
+        String accessToken = jwtProvider.issueAccessToken(99999999L);
+
+        Map<String, Object> requestBody = Map.of("content", "댓글이에요",
+            "secret", "true");
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("articleId", 1)
+            .body(requestBody)
+
+            .when()
+            .post("/{articleId}/comment")
+
+            .then()
+            .statusCode(MEMBER_NOT_FOUND.getStatus().value())
+            .body("errorCode", equalTo(MEMBER_NOT_FOUND.getErrorCode()))
+            .body("message", equalTo(MEMBER_NOT_FOUND.getErrorMessage()));
+    }
+
+    @Test
+    void 필수값이_비어있을_경우_예외가_발생한다() {
+        String accessToken = jwtProvider.issueAccessToken(1L);
+
+        Map<String, Object> requestBody = Map.of("content", "");
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("articleId", 1)
+            .body(requestBody)
+
+            .when()
+            .post("/{articleId}/comment")
+
+            .then()
+            .statusCode(INVALID_REQUEST.getStatus().value())
+            .body("errorCode", equalTo(INVALID_REQUEST.getErrorCode()))
+            .body("message", equalTo(INVALID_REQUEST.getErrorMessage()))
+            .body("detail.content", equalTo("내용은 필수입니다."))
+            .body("detail.secret", equalTo("내용은 필수입니다."));
+    }
+
+    @Test
+    void articleId가_Null이면_예외가_발생한다() {
+        String accessToken = jwtProvider.issueAccessToken(1L);
+
+        Map<String, Object> requestBody = Map.of("content", "댓글이에요",
+            "secret", "true");
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("articleId", " ")
+            .body(requestBody)
+
+            .when()
+            .post("/{articleId}/comment")
+            .then()
+            .statusCode(INVALID_REQUEST.getStatus().value())
+            .body("errorCode", equalTo(INVALID_REQUEST.getErrorCode()))
+            .body("message", equalTo("registerComment.articleId: ID가 비어있습니다."));
+    }
+
+    @Test
+    void articleId가_음수이면_예외가_발생한다() {
+        String accessToken = jwtProvider.issueAccessToken(1L);
+
+        Map<String, Object> requestBody = Map.of("content", "댓글이에요",
+            "secret", "true");
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("articleId", -1)
+            .body(requestBody)
+
+            .when()
+            .post("/{articleId}/comment")
+            .then()
+            .statusCode(INVALID_REQUEST.getStatus().value())
+            .body("errorCode", equalTo(INVALID_REQUEST.getErrorCode()))
+            .body("message", equalTo("registerComment.articleId: 게시글 ID는 양수이어야 합니다."));
+    }
+
+    @Test
+    void articleId가_존재하지_않는_경우_예외가_발생한다() {
+        String accessToken = jwtProvider.issueAccessToken(1L);
+
+        Map<String, Object> requestBody = Map.of("content", "댓글이에요",
+            "secret", "true");
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("articleId", 99999999)
+            .body(requestBody)
+
+            .when()
+            .post("/{articleId}/comment")
+            .then()
+            .statusCode(ARTICLE_NOT_FOUND.getStatus().value())
+            .body("errorCode", equalTo(ARTICLE_NOT_FOUND.getErrorCode()))
+            .body("message", equalTo(ARTICLE_NOT_FOUND.getErrorMessage()));
+    }
+
+    @Test
+    void articleId가_삭제된_경우_예외가_발생한다() {
+        String accessToken = jwtProvider.issueAccessToken(1L);
+
+        Map<String, Object> requestBody = Map.of("content", "댓글이에요",
+            "secret", "true");
+
+        given(documentationSpec)
+            .contentType(APPLICATION_JSON_VALUE)
+            .header(AUTHORIZATION, accessToken)
+            .pathParam("articleId", 13)
+            .body(requestBody)
+
+            .when()
+            .post("/{articleId}/comment")
+            .then()
+            .statusCode(ARTICLE_NOT_FOUND.getStatus().value())
+            .body("errorCode", equalTo(ARTICLE_NOT_FOUND.getErrorCode()))
+            .body("message", equalTo(ARTICLE_NOT_FOUND.getErrorMessage()));
+    }
 }
