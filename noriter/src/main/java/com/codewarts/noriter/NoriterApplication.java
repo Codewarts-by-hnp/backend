6833e9d28d17062d5af3eed82ae89eaa21fc75ff
rkolx <olx.rko.o@gmail.com>
2023-02-27 15:21:31 +0900

    :recycle: NoriterApplication 어노테이션 정리
    - ConfigurationPropertiesScan -> EnableConfigurationProperties 변경
    - EnableConfigurationProperties, EnableWebMvc 어노테이션 Webconfig로 이동
    
    Co-authored-by: PhilSoGooood <peelhw@gmail.com>

--- noriter/src/main/java/com/codewarts/noriter/NoriterApplication.java
+++ noriter/src/main/java/com/codewarts/noriter/NoriterApplication.java
@@ -2,12 +2,8 @@ package com.codewarts.noriter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
-@EnableWebMvc
 public class NoriterApplication {
 
 	public static void main(String[] args) {
--- noriter/src/main/java/com/codewarts/noriter/common/config/WebConfig.java
+++ noriter/src/main/java/com/codewarts/noriter/common/config/WebConfig.java
@@ -1,23 +1,30 @@
 package com.codewarts.noriter.common.config;
 
 
+import com.codewarts.noriter.auth.jwt.JwtProperties;
 import com.codewarts.noriter.auth.jwt.JwtProvider;
+import com.codewarts.noriter.auth.oauth.properties.OAuthPropertiesMapper;
 import com.codewarts.noriter.common.converter.StatusTypeConverter;
 import com.codewarts.noriter.interceptor.AuthVerificationInterceptor;
 import io.netty.resolver.DefaultAddressResolverGroup;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import reactor.netty.http.client.HttpClient;
 
+
 @Configuration
 @RequiredArgsConstructor
+@EnableWebMvc
+@EnableConfigurationProperties({JwtProperties.class, OAuthPropertiesMapper.class})
 public class WebConfig implements WebMvcConfigurer {
 
     private final JwtProvider jwtProvider;
