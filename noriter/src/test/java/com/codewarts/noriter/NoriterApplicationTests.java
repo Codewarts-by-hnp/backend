e135a966b410aee8a7b6debb8b24ed590d9f8ddb
PhilSoGooood <peelhw@gmail.com>
2023-03-03 19:22:33 +0900

    :ambulance: 중복되는 테스트 어노테이션 삭제
    - SpringBootTest 어노테이션 삭제
    
    Co-authored-by: rkolx <olx.rko.o@gmail.com>

--- noriter/src/test/java/com/codewarts/noriter/NoriterApplicationTests.java
+++ noriter/src/test/java/com/codewarts/noriter/NoriterApplicationTests.java
@@ -1,9 +1,7 @@
 package com.codewarts.noriter;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class NoriterApplicationTests {
 
 	@Test
