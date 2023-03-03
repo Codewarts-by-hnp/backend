4d960e0bb538003a5f615a2e95d2164a310e34c2
PhilSoGooood <peelhw@gmail.com>
2023-03-03 19:12:22 +0900

    :recycle: 불필요한 메서드 삭제
    Co-authored-by: rkolx <olx.rko.o@gmail.com>

--- noriter/src/main/java/com/codewarts/noriter/article/repository/GatheringRepository.java
+++ noriter/src/main/java/com/codewarts/noriter/article/repository/GatheringRepository.java
@@ -1,17 +1,8 @@
 package com.codewarts.noriter.article.repository;
 
 import com.codewarts.noriter.article.domain.Gathering;
-import com.codewarts.noriter.article.domain.type.StatusType;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface GatheringRepository extends JpaRepository<Gathering, Long>, CustomArticleRepository {
 
-    @Query("select s from Gathering s where s.status = :status")
-    List<Gathering> findByGatheringCompleted(@Param("status") StatusType status);
-
-    @Query("select s from Gathering s")
-    List<Gathering> findAllGathering();
 }
