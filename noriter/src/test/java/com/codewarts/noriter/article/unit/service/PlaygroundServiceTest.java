0d9b048d94ac28072a8690b1d0d0fa7238599ee6
PhilSoGooood <peelhw@gmail.com>
2023-03-21 17:23:17 +0900

    :recycle: PlaygroundService 테스트 리뷰 반영
    - SoftAssertions 사용
    - 기존 존재하지 않는 회원 및 글 조회시 9999L라고 하드코딩하던 것을 NON_EXIST_INDEX라고 상수 처리
    
    Co-authored-by: rkolx <olx.rko.o@gmail.com>

--- noriter/src/test/java/com/codewarts/noriter/article/unit/service/PlaygroundServiceTest.java
+++ noriter/src/test/java/com/codewarts/noriter/article/unit/service/PlaygroundServiceTest.java
@@ -1,6 +1,7 @@
 package com.codewarts.noriter.article.unit.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.codewarts.noriter.article.domain.Article;
 import com.codewarts.noriter.article.dto.article.ArticleListResponse;
@@ -16,7 +17,7 @@ import com.codewarts.noriter.exception.type.MemberExceptionType;
 import com.codewarts.noriter.member.domain.Member;
 import com.codewarts.noriter.member.repository.MemberRepository;
 import java.util.List;
-import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,6 +36,7 @@ class PlaygroundServiceTest {
     @Autowired
     MemberRepository memberRepository;
 
+    private final Long NON_EXIST_INDEX = Long.MAX_VALUE;
     private Long writerId;
 
     @BeforeEach
@@ -56,10 +58,13 @@ class PlaygroundServiceTest {
             .orElseThrow(() -> new GlobalNoriterException(ArticleExceptionType.ARTICLE_NOT_FOUND));
 
         // then
-        assertThat(article.getId()).isEqualTo(articleId);
-        assertThat(article.getTitle()).isEqualTo(request.getTitle());
-        assertThat(article.getContent()).isEqualTo(request.getContent());
-        assertThat(article.getWriter().getId()).isEqualTo(writerId);
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(article.getId()).isEqualTo(articleId);
+            softAssertions.assertThat(article.getId()).isEqualTo(articleId);
+            softAssertions.assertThat(article.getTitle()).isEqualTo(request.getTitle());
+            softAssertions.assertThat(article.getContent()).isEqualTo(request.getContent());
+            softAssertions.assertThat(article.getWriter().getId()).isEqualTo(writerId);
+        });
     }
 
     @DisplayName("존재하지 않는 회원이 자유게시판 글을 생성 요청하면 예외를 발생시킨다.")
@@ -69,7 +74,7 @@ class PlaygroundServiceTest {
         PlaygroundCreateRequest request = new PlaygroundCreateRequest("테스트 제목", "테스트 내용", null);
 
         // expected
-        Assertions.assertThatThrownBy(() -> playgroundService.create(request, 99999L))
+        assertThatThrownBy(() -> playgroundService.create(request, NON_EXIST_INDEX))
             .message()
             .isEqualTo(MemberExceptionType.MEMBER_NOT_FOUND.getErrorMessage());
     }
@@ -85,17 +90,19 @@ class PlaygroundServiceTest {
         PlaygroundDetailResponse response = playgroundService.findDetail(articleId, writerId);
 
         // then
-        assertThat(response.getId()).isEqualTo(articleId);
-        assertThat(response.getTitle()).isEqualTo(request.getTitle());
-        assertThat(response.getContent()).isEqualTo(request.getContent());
-        assertThat(response.getWriter().getId()).isEqualTo(writerId);
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(response.getId()).isEqualTo(articleId);
+            softAssertions.assertThat(response.getTitle()).isEqualTo(request.getTitle());
+            softAssertions.assertThat(response.getContent()).isEqualTo(request.getContent());
+            softAssertions.assertThat(response.getWriter().getId()).isEqualTo(writerId);
+        });
     }
 
     @DisplayName("존재하지 않는 자유게시판 글을 상세 조회 요청하면 예외를 발생시킨다.")
     @Test
     void findDetailWithWrongArticleIdThenThrow() {
         // expected
-        Assertions.assertThatThrownBy(() -> playgroundService.findDetail(9999L, writerId))
+        assertThatThrownBy(() -> playgroundService.findDetail(NON_EXIST_INDEX, writerId))
             .message()
             .isEqualTo(ArticleExceptionType.ARTICLE_NOT_FOUND.getErrorMessage());
     }
@@ -111,7 +118,7 @@ class PlaygroundServiceTest {
         playgroundService.delete(articleId, writerId);
 
         // expected
-        Assertions.assertThatThrownBy(() -> playgroundService.findDetail(articleId, writerId))
+        assertThatThrownBy(() -> playgroundService.findDetail(articleId, writerId))
             .message()
             .isEqualTo(ArticleExceptionType.DELETED_ARTICLE.getErrorMessage());
     }
@@ -176,11 +183,13 @@ class PlaygroundServiceTest {
         PlaygroundDetailResponse response = playgroundService.findDetail(articleId, writerId);
 
         // then
-        assertThat(response.getId()).isEqualTo(articleId);
-        assertThat(response.getTitle()).isEqualTo(updateRequest.getTitle());
-        assertThat(response.getContent()).isEqualTo(updateRequest.getContent());
-        assertThat(response.getHashtags()).hasSize(updateRequest.getHashtags().size());
-        assertThat(response.getWriter().getId()).isEqualTo(writerId);
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(response.getId()).isEqualTo(articleId);
+            softAssertions.assertThat(response.getTitle()).isEqualTo(updateRequest.getTitle());
+            softAssertions.assertThat(response.getContent()).isEqualTo(updateRequest.getContent());
+            softAssertions.assertThat(response.getHashtags()).hasSize(updateRequest.getHashtags().size());
+            softAssertions.assertThat(response.getWriter().getId()).isEqualTo(writerId);
+        });
     }
 
     @DisplayName("존재하지 않는 자유게시판 글을 수정 요청하는 경우 예외를 발생시킨다.")
@@ -195,8 +204,8 @@ class PlaygroundServiceTest {
             List.of("수정된 해시태그"));
 
         // expected
-        Assertions.assertThatThrownBy(
-                () -> playgroundService.update(99999L, updateRequest, writerId))
+        assertThatThrownBy(
+                () -> playgroundService.update(NON_EXIST_INDEX, updateRequest, writerId))
             .message()
             .isEqualTo(ArticleExceptionType.ARTICLE_NOT_FOUND.getErrorMessage());
     }
@@ -213,8 +222,8 @@ class PlaygroundServiceTest {
             List.of("수정된 해시태그"));
 
         // expected
-        Assertions.assertThatThrownBy(
-                () -> playgroundService.update(articleId, updateRequest, 9999L))
+        assertThatThrownBy(
+                () -> playgroundService.update(articleId, updateRequest, NON_EXIST_INDEX))
             .message()
             .isEqualTo(MemberExceptionType.MEMBER_NOT_FOUND.getErrorMessage());
     }
@@ -236,7 +245,7 @@ class PlaygroundServiceTest {
             List.of("수정된 해시태그"));
 
         // expected
-        Assertions.assertThatThrownBy(
+        assertThatThrownBy(
                 () -> playgroundService.update(articleId, updateRequest, wrongWriterId))
             .message()
             .isEqualTo(ArticleExceptionType.ARTICLE_NOT_MATCHED_WRITER.getErrorMessage());
@@ -255,7 +264,7 @@ class PlaygroundServiceTest {
             List.of("수정된 해시태그"));
 
         // expected
-        Assertions.assertThatThrownBy(
+        assertThatThrownBy(
                 () -> playgroundService.update(articleId, updateRequest, writerId))
             .message()
             .isEqualTo(ArticleExceptionType.DELETED_ARTICLE.getErrorMessage());
@@ -274,7 +283,7 @@ class PlaygroundServiceTest {
         Article article = articleRepository.findById(articleId).get();
 
         // then
-        Assertions.assertThat(article.isDeleted()).isTrue();
+        assertThat(article.isDeleted()).isTrue();
     }
 
     @DisplayName("존재하지 않는 회원이 자유게시판 글을 삭제 요청하는 경우 예외를 발생시킨다.")
@@ -286,7 +295,7 @@ class PlaygroundServiceTest {
         Long articleId = playgroundService.create(createRequest, writerId);
 
         // expected
-        Assertions.assertThatThrownBy(() -> playgroundService.delete(articleId, 9999L))
+        assertThatThrownBy(() -> playgroundService.delete(articleId, NON_EXIST_INDEX))
             .message()
             .isEqualTo(MemberExceptionType.MEMBER_NOT_FOUND.getErrorMessage());
     }
@@ -304,7 +313,7 @@ class PlaygroundServiceTest {
         Long articleId = playgroundService.create(createRequest, writerId);
 
         // expected
-        Assertions.assertThatThrownBy(() -> playgroundService.delete(articleId, wrongWriterId))
+        assertThatThrownBy(() -> playgroundService.delete(articleId, wrongWriterId))
             .message()
             .isEqualTo(ArticleExceptionType.ARTICLE_NOT_MATCHED_WRITER.getErrorMessage());
     }
@@ -315,10 +324,10 @@ class PlaygroundServiceTest {
         // when
         PlaygroundCreateRequest createRequest = new PlaygroundCreateRequest("테스트 제목1", "테스트 내용1",
             List.of("해시태그1", "해시태그2"));
-        Long articleId = playgroundService.create(createRequest, writerId);
+        playgroundService.create(createRequest, writerId);
 
         // expected
-        Assertions.assertThatThrownBy(() -> playgroundService.delete(99999L, writerId))
+        assertThatThrownBy(() -> playgroundService.delete(NON_EXIST_INDEX, writerId))
             .message()
             .isEqualTo(ArticleExceptionType.ARTICLE_NOT_FOUND.getErrorMessage());
     }
@@ -335,7 +344,7 @@ class PlaygroundServiceTest {
         playgroundService.delete(articleId, writerId);
 
         // expected
-        Assertions.assertThatThrownBy(() -> playgroundService.delete(articleId, writerId))
+        assertThatThrownBy(() -> playgroundService.delete(articleId, writerId))
             .message()
             .isEqualTo(ArticleExceptionType.DELETED_ARTICLE.getErrorMessage());
     }
